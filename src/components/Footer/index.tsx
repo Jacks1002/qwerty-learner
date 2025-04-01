b02c46fd7c7de611d8c09d69577c129910ec3f0c
Jacks1002 <133033178+Jacks1002@users.noreply.github.com>
2025-04-01 20:09:15 +0800

    test2

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,48 @@
+import { DonatingCard } from '../DonatingCard'
+import { StickerButton } from '../DonatingCard/components/StickerButton'
+import redBookCode from '@/assets/redBook-code.jpg'
+import InfoPanel from '@/components/InfoPanel'
+import Tooltip from '@/components/Tooltip'
+import { infoPanelStateAtom } from '@/store'
+import type { InfoPanelType } from '@/typings'
+import { recordOpenInfoPanelAction } from '@/utils'
+import { useAtom } from 'jotai'
+import type React from 'react'
+import { useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
+import IconMail from '~icons/material-symbols/mail'
+import IconCoffee2 from '~icons/mdi/coffee'
+import IconXiaoHongShu from '~icons/my-icons/xiaohongshu'
+import RiLinksLine from '~icons/ri/links-line'
+import IconTwitter from '~icons/ri/twitter-fill'
+import IconGithub from '~icons/simple-icons/github'
+import IconVisualstudiocode from '~icons/simple-icons/visualstudiocode'
+import IconWechat2 from '~icons/simple-icons/wechat'
+import IconWechat from '~icons/tabler/brand-wechat'
+import IconCoffee from '~icons/tabler/coffee'
+import IconTerminal2 from '~icons/tabler/terminal-2'
+import IconFlagChina from '~icons/twemoji/flag-china'
+
+const Footer: React.FC = () => {
+  const [infoPanelState, setInfoPanelState] = useAtom(infoPanelStateAtom)
+  const navigate = useNavigate()
+
+  const handleOpenInfoPanel = useCallback(
+    (modalType: InfoPanelType) => {
+      recordOpenInfoPanelAction(modalType, 'footer')
+      setInfoPanelState((state) => ({ ...state, [modalType]: true }))
+    },
+    [setInfoPanelState],
+  )
+
+  const handleCloseInfoPanel = useCallback(
+    (modalType: InfoPanelType) => {
+      setInfoPanelState((state) => ({ ...state, [modalType]: false }))
+    },
+    [setInfoPanelState],
+  )
+
+  return ('');
+}
+
+export default Footer

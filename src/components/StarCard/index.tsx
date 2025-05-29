d12ab4985bc66b874ae65a2ff43c2608a8cc9ab3
Jacks1002 <133033178+Jacks1002@users.noreply.github.com>
2025-05-29 21:22:49 +0800

    fix a bug

--- src/components/StarCard/index.tsx
+++ src/components/StarCard/index.tsx
@@ -52,7 +52,72 @@ export default function StarCard() {
   }, [isCounting, countdown, setIsShow])
 
   const content = useMemo(() => {
-    return ('')
+    return (
+      <>
+        {isCounting ? (
+          <div className="mt-6 flex w-full flex-col items-center gap-4">
+            <div className="flex max-w-full items-center text-sm">
+              <div className="flex h-7 min-w-[12rem] items-center justify-between rounded-full bg-gray-100 pl-5 text-black dark:bg-zinc-900 dark:text-white">
+                <div className="flex-0 min-w-0 overflow-hidden text-ellipsis whitespace-nowrap">{`${
+                  location.hostname
+                }${location.pathname.replace(/\/$/, '')}`}</div>
+                <IconStarOutline className="ml-4 mr-2 h-4 w-4 flex-shrink-0 text-neutral-400" />
+              </div>
+              <div className="flex flex-shrink-0 items-center">
+                <div className="ml-0.5">👈</div>
+                <IconStar className="h-4 w-4 text-indigo-600" />
+                <div className="ml-1 text-gray-600 dark:text-gray-300">点亮它！</div>
+              </div>
+            </div>
+            <span className="w-full text-center text-gray-600 dark:text-gray-300">
+              收藏快捷键<span className="ml-2 text-indigo-600 dark:text-indigo-500">{IS_MAC_OS ? '⌘' : 'Ctrl'} + D</span>
+            </span>
+          </div>
+        ) : (
+          <div className="flex pb-0 pt-6">
+            <button
+              className="rounded-lg bg-indigo-600 px-6 py-2 text-lg text-white transition-colors duration-300 focus:outline-none"
+              type="button"
+              onClick={onClickWantStar}
+              title="我想收藏"
+            >
+              我想收藏
+            </button>
+          </div>
+        )}
+      </>
+    )
+  }, [isCounting, onClickWantStar])
 
-  return ('')
+  return (
+    <Transition
+      appear
+      show={isShow}
+      enter="transition ease-out duration-300 transform"
+      enterFrom="translate-x-full -translate-y-full"
+      enterTo="translate-x-0 translate-y-0"
+      leave="transition ease-in duration-500 transform"
+      leaveFrom="translate-x-0 translate-y-0"
+      leaveTo="translate-x-full -translate-y-full"
+      className="fixed inset-0 z-30 hidden h-0 justify-center md:flex"
+    >
+      <div className="fixed right-1 top-4 flex w-150 flex-col items-center justify-evenly rounded-2xl bg-white p-12 shadow-2xl dark:bg-gray-800">
+        <div className="absolute right-3 top-3 flex h-5 items-center">
+          {isCounting && (
+            <span className="mx-1.5 dark:text-gray-100">
+              <span className="text-indigo-600">{countdown}s </span>
+              后自动关闭
+            </span>
+          )}
+          <button type="button" onClick={onClickCloseStar} title="关闭提示" aria-label="关闭提示">
+            <IconCircleX className="h-5 w-5 text-indigo-400" />
+          </button>
+        </div>
+        <span className="pb-4 text-xl text-gray-600 dark:text-gray-50">
+          坚持练习，提高语言能力。将 <span className="text-indigo-600">「Qwerty Learner」</span>保存到收藏夹，永不迷失！
+        </span>
+        {content}
+      </div>
+    </Transition>
+  )
 }
